760e897ce37f3064218e07651063705754d1a21e
cqm <mtdxc@126.com>
2022-06-08 10:41:54 +0800

    aac转码使用原track的通道

--- src/Common/MultiMediaSourceMuxer.cpp
+++ src/Common/MultiMediaSourceMuxer.cpp
@@ -331,7 +331,7 @@ bool MultiMediaSourceMuxer::onTrackReady(const Track::Ptr &track) {
             _audio_dec = nullptr;
             _audio_enc = nullptr;
             if (rtmp) {
-                Track::Ptr newTrack(new AACTrack(44100, 1));
+                Track::Ptr newTrack(new AACTrack(44100, std::dynamic_pointer_cast<AudioTrack>(track)->getAudioChannel()));
                 GET_CONFIG(int, bitrate, General::kAacBitrate);
                 newTrack->setBitRate(bitrate);
                 rtmp->addTrack(newTrack);
